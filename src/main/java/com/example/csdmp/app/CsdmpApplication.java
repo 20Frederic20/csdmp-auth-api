85588820368195e669680c97c1dc4c855a42c4d9
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-01 20:21:58 +0100

    initial commit

---
+++ src/main/java/com/example/csdmp/app/CsdmpApplication.java
@@ -0,0 +1,13 @@
+package com.example.csdmp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsdmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsdmpApplication.class, args);
+	}
+
+}
