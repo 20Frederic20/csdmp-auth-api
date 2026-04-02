ec8b9fa1fa25d4f1c7cb25ee476568bb6bba2461
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-03 00:15:24 +0100

    getter and setter defined in permission entity

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/entities/PermissionEntity.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/entities/PermissionEntity.java
@@ -23,4 +23,24 @@ public class PermissionEntity {
     private String action;
 
     private String description;
+
+    public PermissionEntity() {}
+
+    public PermissionEntity(UUID id, String resource, String action, String description) {
+        this.id = id;
+        this.resource = resource;
+        this.action = action;
+        this.description = description;
+    }
+
+    // Getters et Setters
+    public UUID getId() { return id; }
+    public void setId(UUID id) { this.id = id; }
+    public String getResource() { return resource; }
+    public void setResource(String resource) { this.resource = resource; }
+    public String getAction() { return action; }
+    public void setAAction(String action) { this.action = action; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
 }
