685cc6b7fca07498e923371ccefc7bf26838ac60
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-02 23:29:32 +0100

    permission: domain repo

---
+++ src/main/java/com/example/csdmp/app/domain/repositories/PermissionRepository.java
@@ -0,0 +1,17 @@
+package com.example.csdmp.app.domain.repositories;
+
+import com.example.csdmp.app.domain.entities.Permission;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PermissionRepository {
+    void save(Permission permission);
+    Optional<Permission> findById(UUID id);
+    Optional<Permission> findBySlug(String resource, String action);
+    List<Permission> findAll();
+    void delete(UUID id);
+}
+
+
