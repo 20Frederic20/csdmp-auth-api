8b3e5e76ba95bbaca82a27fd0a9b4fbc9eed63b1
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-03 19:06:38 +0100

    permissions correction creation return now 201

--- src/main/java/com/example/csdmp/app/interfaces/rest/controllers/PermissionController.java
+++ src/main/java/com/example/csdmp/app/interfaces/rest/controllers/PermissionController.java
@@ -5,10 +5,11 @@ import com.example.csdmp.app.domain.services.PermissionService;
 import com.example.csdmp.app.interfaces.rest.dtos.PermissionRequest;
 import com.example.csdmp.app.interfaces.rest.dtos.PermissionResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/permissions")
@@ -20,9 +21,39 @@ public class PermissionController {
         this.permissionService = permissionService;
     }
 
+    @GetMapping()
+    public ResponseEntity<List<PermissionResponse>> all() {
+        List<Permission> permissions = permissionService.getAll();
+
+        List<PermissionResponse> response = permissions.stream()
+                .map(permission -> new PermissionResponse(
+                        permission.getId(),
+                        permission.getResource(),
+                        permission.getAction(),
+                        permission.getDescription()
+                ))
+                .toList();
+
+        return ResponseEntity.ok(response);
+    }
+
     @PostMapping()
     public ResponseEntity<PermissionResponse> create(@RequestBody PermissionRequest request) {
         Permission created = permissionService.create(request.resource(), request.action(), request.description());
-        return ResponseEntity.ok(new PermissionResponse(created.getId(), created.getResource(), created.getAction(), created.getDescription()));
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(
+                new PermissionResponse(
+                        created.getId(),
+                        created.getResource(),
+                        created.getAction(),
+                        created.getDescription()
+                )
+        );
     }
+
+
 }
