c8b50df3c4a407e09d3790dd2b8aa2bf3468d1e9
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-03 02:08:19 +0100

    permission bean config

--- src/main/java/com/example/csdmp/app/infrastructure/domain/DomainConfig.java
+++ src/main/java/com/example/csdmp/app/infrastructure/domain/DomainConfig.java
@@ -1,6 +1,8 @@
 package com.example.csdmp.app.infrastructure.domain;
 
+import com.example.csdmp.app.domain.repositories.PermissionRepository;
 import com.example.csdmp.app.domain.repositories.RoleRepository;
+import com.example.csdmp.app.domain.services.PermissionService;
 import com.example.csdmp.app.domain.services.RoleService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +12,10 @@ public class DomainConfig {
     @Bean
     public RoleService roleService(RoleRepository roleRepository) {
         return new RoleService(roleRepository);
+    }
+
+    @Bean
+    public PermissionService permmissionService(PermissionRepository permissionRepository) {
+        return new PermissionService(permissionRepository);
     }
 }
