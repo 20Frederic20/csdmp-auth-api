075190883b044863ebc464bdafa4dc2b218d931a
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-03 01:53:02 +0100

    correction on implementation: older is role instead of permissionentity

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaPermissionRepository.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaPermissionRepository.java
@@ -1,12 +1,11 @@
 package com.example.csdmp.app.infrastructure.persistence.repositories;
 
 import com.example.csdmp.app.infrastructure.persistence.entities.PermissionEntity;
-import com.example.csdmp.app.infrastructure.persistence.entities.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface JpaPermissionRepository extends JpaRepository<RoleEntity, UUID> {
+public interface JpaPermissionRepository extends JpaRepository<PermissionEntity, UUID> {
     Optional<PermissionEntity> findBySlug(String resource, String action);
 }
