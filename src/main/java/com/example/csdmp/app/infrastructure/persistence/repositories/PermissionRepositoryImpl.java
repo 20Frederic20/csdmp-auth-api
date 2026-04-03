12e2de58ee8cfd6308f40b003099127febe7e5c5
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-03 01:54:00 +0100

    implementation of permission repository

---
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/PermissionRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.example.csdmp.app.infrastructure.persistence.repositories;
+
+import com.example.csdmp.app.domain.entities.Permission;
+import com.example.csdmp.app.domain.repositories.PermissionRepository;
+import com.example.csdmp.app.infrastructure.persistence.mappers.PermissionMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class PermissionRepositoryImpl implements PermissionRepository {
+    private final JpaPermissionRepository jpaPermissionRepository;
+
+    public PermissionRepositoryImpl(JpaPermissionRepository jpaPermissionRepository) {
+        this.jpaPermissionRepository = jpaPermissionRepository;
+    }
+
+    @Override
+    public void save(Permission permission) {
+        jpaPermissionRepository.save(PermissionMapper.toEntity(permission));
+    }
+
+    @Override
+    public Optional<Permission> findById(UUID id) {
+        return jpaPermissionRepository.findById(id).map(PermissionMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Permission> findBySlug(String resource, String action) {
+        return jpaPermissionRepository.findBySlug(resource, action).map(PermissionMapper::toDomain);
+    }
+
+    @Override
+    public List<Permission> findAll(){
+        return jpaPermissionRepository.findAll().stream().map(PermissionMapper::toDomain).toList();
+    }
+
+    @Override
+    public void delete(UUID id){
+        jpaPermissionRepository.deleteById(id);
+    }
+
+}
