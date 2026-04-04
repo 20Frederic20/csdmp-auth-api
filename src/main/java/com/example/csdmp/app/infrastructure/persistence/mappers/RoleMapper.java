47b96cbbe9d9e4bed10716b702496b90b3656548
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-04 10:35:33 +0100

    add into role entity creation of permissions or returning of permissions to create role_permissions data

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/mappers/RoleMapper.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/mappers/RoleMapper.java
@@ -2,19 +2,32 @@ package com.example.csdmp.app.infrastructure.persistence.mappers;
 
 import com.example.csdmp.app.domain.entities.Permission;
 import com.example.csdmp.app.domain.entities.Role;
+import com.example.csdmp.app.infrastructure.persistence.entities.PermissionEntity;
 import com.example.csdmp.app.infrastructure.persistence.entities.RoleEntity;
 
 import java.util.List;
 
 public class RoleMapper {
     public static RoleEntity toEntity(Role domain) {
-        return new RoleEntity(domain.getId(), domain.getName(), domain.getDescription());
+        RoleEntity entity = new RoleEntity(domain.getId(), domain.getName(), domain.getDescription());
+
+        if (domain.getPermissions() != null) {
+            List<PermissionEntity> permissionEntities = domain.getPermissions().stream()
+                    .map(PermissionMapper::toEntity)
+                    .toList();
+            entity.setPermissions(permissionEntities);
+        }
+
+        return entity;
     }
 
     public static Role toDomain(RoleEntity entity) {
-        List<Permission> domainPermissions = entity.getPermissions().stream()
-                .map(PermissionMapper::toDomain)
-                .toList();
+        List<Permission> domainPermissions = (entity.getPermissions() == null)
+                ? List.of()
+                : entity.getPermissions().stream()
+                  .map(PermissionMapper::toDomain)
+                  .toList();
+
         return new Role(entity.getId(), entity.getName(), entity.getDescription(), domainPermissions);
     }
 }
