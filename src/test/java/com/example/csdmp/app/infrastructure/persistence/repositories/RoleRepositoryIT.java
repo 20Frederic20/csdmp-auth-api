c0e5dbe15efcc668afa03126a1c4b08c1a9e9387
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-04 10:36:21 +0100

    role repo test

---
+++ src/test/java/com/example/csdmp/app/infrastructure/persistence/repositories/RoleRepositoryIT.java
@@ -0,0 +1,48 @@
+    package com.example.csdmp.app.infrastructure.persistence.repositories;
+
+    import com.example.csdmp.app.domain.entities.Permission;
+    import com.example.csdmp.app.domain.entities.Role;
+    import com.example.csdmp.app.infrastructure.persistence.entities.PermissionEntity;
+    import org.junit.jupiter.api.Test;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+    import org.springframework.context.annotation.Import;
+
+    import java.util.List;
+    import java.util.Optional;
+    import java.util.UUID;
+
+    import static org.assertj.core.api.Assertions.assertThat;
+
+    @DataJpaTest // Charge uniquement la couche JPA (EntityManager, Repositories)
+    @Import({RoleRepositoryImpl.class, PermissionRepositoryImpl.class}) // On importe nos implémentations réelles
+    class RoleRepositoryIT {
+
+        @Autowired
+        private RoleRepositoryImpl roleRepository;
+
+        @Autowired
+        private JpaPermissionRepository jpaPermissionRepository;
+
+        @Test
+        void should_save_role_with_permissions_in_database() {
+            UUID permId = UUID.randomUUID();
+            PermissionEntity permEntity = new PermissionEntity(permId, "patients", "create", "Desc");
+            jpaPermissionRepository.save(permEntity);
+
+            Permission domainPerm = new Permission(permId, "patients", "create", "Desc");
+            Role role = new Role(UUID.randomUUID(), "ADMIN", "Super Admin", List.of(domainPerm));
+
+            roleRepository.save(role);
+
+            Optional<Role> savedRole = roleRepository.findByName("ADMIN");
+
+            assertThat(savedRole).isPresent();
+            assertThat(savedRole.get().getName()).isEqualTo("ADMIN");
+
+            assertThat(savedRole.get().getPermissions()).hasSize(1);
+            assertThat(
+                    savedRole.get().getPermissions().get(0).getResource() + ":" + savedRole.get().getPermissions().get(0).getAction()
+            ).isEqualTo("patients:create");
+        }
+    }
