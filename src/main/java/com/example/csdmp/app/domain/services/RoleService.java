3f8f893cc237996ed2aa4aa35ceed8f5cd8b3a60
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-04 02:48:43 +0100

    mapper permissions in RoleService

--- src/main/java/com/example/csdmp/app/domain/services/RoleService.java
+++ src/main/java/com/example/csdmp/app/domain/services/RoleService.java
@@ -1,24 +1,34 @@
 package com.example.csdmp.app.domain.services;
 
+import com.example.csdmp.app.domain.entities.Permission;
 import com.example.csdmp.app.domain.entities.Role;
+import com.example.csdmp.app.domain.repositories.PermissionRepository;
 import com.example.csdmp.app.domain.repositories.RoleRepository;
 
 
+import java.util.ArrayList;
 import java.util.UUID;
 import java.util.List;
 
 public class RoleService {
     private final RoleRepository roleRepository;
+    private final PermissionRepository permissionRepository;
 
-    public RoleService(RoleRepository roleRepository) {
+    public RoleService(
+            RoleRepository roleRepository, PermissionRepository permissionRepository
+    ) {
         this.roleRepository = roleRepository;
+        this.permissionRepository = permissionRepository;
     }
 
-    public Role create(String name, String description) {
+    public Role create(String name, String description, List<UUID> permissionIds) {
         if (roleRepository.findByName(name).isPresent()) {
             throw new RuntimeException("Le rôle existe déjà");
         }
-        Role role = new Role(UUID.randomUUID(), name, description);
+        List<Permission> permissions = permissionIds.stream()
+                .map(pId -> permissionRepository.findById(pId).orElseThrow())
+                .toList();
+        Role role = new Role(UUID.randomUUID(), name, description, permissions);
         roleRepository.save(role);
         return role;
     }
@@ -27,11 +37,14 @@ public class RoleService {
         return roleRepository.findAll();
     }
 
-    public Role update(UUID id, String newName, String newDescription) {
+    public Role update(UUID id, String newName, String newDescription, List<UUID> newPermissionIds) {
         Role existingRole = roleRepository.findById(id).orElseThrow(
                 () -> new RuntimeException("Rôle non trouvé avec l'ID : " + id)
         );
-        Role updatedRole = new Role(existingRole.getId(), newName, newDescription);
+        List<Permission> newPermissions = newPermissionIds.stream()
+                .map(pId -> permissionRepository.findById(pId).orElseThrow())
+                .toList();
+        Role updatedRole = new Role(existingRole.getId(), newName, newDescription, newPermissions);
 
         roleRepository.save(updatedRole);
         return updatedRole;
