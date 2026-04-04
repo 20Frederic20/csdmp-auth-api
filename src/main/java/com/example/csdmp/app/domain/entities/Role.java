fca9b5095e05a54bc3f62d4a5c77f6a86145e2b1
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-04 02:46:53 +0100

    add permissions to roles entity

--- src/main/java/com/example/csdmp/app/domain/entities/Role.java
+++ src/main/java/com/example/csdmp/app/domain/entities/Role.java
@@ -1,17 +1,23 @@
 package com.example.csdmp.app.domain.entities;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class Role {
     private final UUID id;
     private final String name;
     private final String description;
+    private final List<Permission> permissions;
 
-    public Role(UUID id, String name, String description){
+    public Role(UUID id, String name, String description, List<Permission> permissions){
             this.id=id;
             this.name=name;
             this.description=description;
+            this.permissions = permissions != null ? permissions : new ArrayList<>();
     }
+    public List<Permission> getPermissions() { return permissions; }
+
 
     public UUID getId(){
         return id;
