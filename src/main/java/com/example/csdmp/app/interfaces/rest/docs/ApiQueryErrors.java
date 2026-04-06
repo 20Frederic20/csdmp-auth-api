bf64a0c94beeef5f315fc29ff254baf35ccb47d8
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-06 01:34:40 +0100

    api query errrors for  get

---
+++ src/main/java/com/example/csdmp/app/interfaces/rest/docs/ApiQueryErrors.java
@@ -0,0 +1,22 @@
+package com.example.csdmp.app.interfaces.rest.docs;
+
+import com.example.csdmp.app.interfaces.rest.exceptions.ErrorResponse;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@ApiResponses({
+        @ApiResponse(responseCode = "404", description = "La ressource demandée n'existe pas",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+        @ApiResponse(responseCode = "500", description = "Erreur technique serveur",
+                content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+})
+public @interface ApiQueryErrors {}
