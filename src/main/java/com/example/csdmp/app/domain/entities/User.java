6873bdd28b36a31d6ab468bb2d52728b91b98dce
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-07 12:29:05 +0100

    User domain entities impl

---
+++ src/main/java/com/example/csdmp/app/domain/entities/User.java
@@ -0,0 +1,47 @@
+package com.example.csdmp.app.domain.entities;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public class User extends BaseEntity {
+    private final UUID id;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String healthId;
+    private final String password;
+
+    public User(UUID id, String firstName, String lastName, String email, String healthId, String password, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt, boolean isActive) {
+        super(isActive, createdAt, updatedAt, deletedAt);
+        this.id=id;
+        this.email=email;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.healthId=healthId;
+        this.password=password;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getHealthId() {
+        return healthId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
