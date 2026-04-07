442595c9a993756ebfd52b8750d84ac3c34cbcab
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-07 13:28:56 +0100

    remove unused import in global exception handler

--- src/main/java/com/example/csdmp/app/interfaces/rest/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/example/csdmp/app/interfaces/rest/exceptions/GlobalExceptionHandler.java
@@ -6,12 +6,10 @@ import com.example.csdmp.app.domain.exceptions.UnauthorizedException;
 import com.example.csdmp.app.domain.exceptions.ValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
