88594f3193a7e09f2114ef35c34b289736351a4e
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-08 20:34:30 +0100

    user create controler

---
+++ src/main/java/com/example/csdmp/app/interfaces/rest/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.csdmp.app.interfaces.rest.controllers;
+
+import com.example.csdmp.app.domain.entities.Role;
+import com.example.csdmp.app.domain.entities.User;
+import com.example.csdmp.app.domain.services.RoleService;
+import com.example.csdmp.app.domain.services.UserService;
+import com.example.csdmp.app.interfaces.rest.docs.ApiMutationErrors;
+import com.example.csdmp.app.interfaces.rest.dtos.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Operation(summary = "Créer un nouveau rôle", description = "Permet d'associer un nom à une liste de permissions")
+    @ApiResponse(responseCode = "201", description = "Rôle créé avec succès")
+    @ApiMutationErrors
+    @PostMapping()
+    public ResponseEntity<UserResponse> create(@RequestBody UserRequest request) {
+        User created = userService.create(request.firstName(), request.lastName(), request.email(), request.healthId(), request.password(), request.isActive());
+        return ResponseEntity.ok(new UserResponse(created.getId(), created.getFirstName(), created.getLastName(), created.getEmail(), created.getHealthId(), created.getPassword(), created.getIsActive(), created.getCreatedAt(), created.getUpdatedAt(), created.getDeletedAt()));
+    }
+}
