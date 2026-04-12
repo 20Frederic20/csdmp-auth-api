18693155492e12b166dcd40dfed2c111f7cfbf29
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-12 21:22:04 +0100

    UserRepositoryIT: should_save_user_in_database: update thee test, should_delete_user_in_database: write test for deleting user

--- src/test/java/com/example/csdmp/app/infrastructure/persistence/repositories/UserRepositoryIT.java
+++ src/test/java/com/example/csdmp/app/infrastructure/persistence/repositories/UserRepositoryIT.java
@@ -3,19 +3,25 @@ package com.example.csdmp.app.infrastructure.persistence.repositories;
 import com.example.csdmp.app.domain.entities.Role;
 import com.example.csdmp.app.domain.entities.User;
 import com.example.csdmp.app.domain.security.PasswordInterface;
+import com.example.csdmp.app.infrastructure.persistence.entities.RoleEntity;
+import com.example.csdmp.app.infrastructure.persistence.entities.UserEntity;
 import com.example.csdmp.app.infrastructure.security.BCryptPasswordHasher;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static java.time.temporal.ChronoUnit.SECONDS;
+import static org.assertj.core.api.Assertions.within;
 
 @DataJpaTest
-@Import({UserRepositoryImpl.class, RoleRepositoryImpl.class, BCryptPasswordHasher.class})
+@Import({RoleRepositoryImpl.class, UserRepositoryImpl.class, BCryptPasswordHasher.class})
 public class UserRepositoryIT {
 
     @Autowired
@@ -31,13 +37,53 @@ public class UserRepositoryIT {
     void should_save_user_in_database() {
         UUID userId = UUID.randomUUID();
         String hashedPassword = passwordInterface.hash("secret12345");
-        User user = new User(userId, "Nevis", "KOULOGBO", "lewisnevis44@gmail.com", "012345678910", hashedPassword, true);
+        List <Role> roles = List.of();
+        User user = new User(userId, "Nevis", "KOULOGBO", "lewisnevis44@gmail.com", "012345678910", hashedPassword, roles, true);
 
         userRepository.save(user);
 
-        Optional<User> savedUser = userRepository.findByEmail("lewisnevis44@gmail.com");
+        Optional<User> savedUser = userRepository.findByEmail("lewisnevis44@gmail.com", false);
 
         assertThat(savedUser).isPresent();
         assertThat(savedUser.get().getPassword()).isEqualTo(hashedPassword);
+    }
+
+    @Test
+    void should_save_user_with_role_in_database() {
+        UUID userId = UUID.randomUUID();
+        UUID roleId = UUID.randomUUID();
+        String hashedPassword = passwordInterface.hash("secret12345");
+        RoleEntity roleEntity = new RoleEntity(roleId, "PATIENT", "");
+        roleRepository.save(roleEntity);
+        Role role = new Role(roleId, "PATIENT", "", List.of());
+        List <Role> roles = List.of(role);
+        User user = new User(userId, "Nevis", "KOULOGBO", "lewisnevis44@gmail.com", "012345678910", hashedPassword, roles, true);
+
+        userRepository.save(user);
+
+        Optional<User> savedUser = userRepository.findByEmail("lewisnevis44@gmail.com", false);
+
+        assertThat(savedUser).isPresent();
+        assertThat(savedUser.get().getPassword()).isEqualTo(hashedPassword);
+        assertThat(savedUser.get().getRoles())
+                .extracting(Role::getName)
+                .containsExactly("PATIENT");
+    }
+
+    @Test
+    void should_delete_user_in_database() {
+        UUID userId = UUID.randomUUID();
+        String hashedPassword = passwordInterface.hash("secret12345");
+        List <Role> roles = List.of();
+        User user = new User(userId, "Nevis", "KOULOGBO", "lewisnevis44@gmail.com", "012345678910", hashedPassword, roles, true);
+
+        userRepository.save(user);
+
+        userRepository.delete(userId);
+
+        Optional<User> deletedUser = userRepository.findByEmail("lewisnevis44@gmail.com", true);
+        assertThat(deletedUser).isPresent();
+        assertThat(deletedUser.get().getDeletedAt())
+                .isCloseTo(LocalDateTime.now(), within(5, SECONDS));
     }
 }
