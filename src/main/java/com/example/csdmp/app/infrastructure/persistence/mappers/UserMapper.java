96a4c9ffe86600716620c81e65fe6c3150913fde
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-12 21:19:38 +0100

    UserMapper: add roles in mapper

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/mappers/UserMapper.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/mappers/UserMapper.java
@@ -1,8 +1,12 @@
 package com.example.csdmp.app.infrastructure.persistence.mappers;
 
+import com.example.csdmp.app.domain.entities.Role;
 import com.example.csdmp.app.domain.entities.User;
+import com.example.csdmp.app.infrastructure.persistence.entities.RoleEntity;
 import com.example.csdmp.app.infrastructure.persistence.entities.UserEntity;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class UserMapper {
@@ -13,6 +17,11 @@ public class UserMapper {
     public static User toDomain(UserEntity entity) {
         if (entity == null) return null;
 
+        List<Role> entityRoles = (entity.getRoles() == null)
+                ? List.of()
+                : entity.getRoles().stream()
+                  .map(RoleMapper::toDomain)
+                  .toList();
         return new User(
                 entity.getId(),
                 entity.getFirstName(),
@@ -23,21 +32,29 @@ public class UserMapper {
                 entity.getIsActive(),
                 entity.getCreatedAt(),
                 entity.getUpdatedAt(),
-                entity.getDeletedAt()
+                entity.getDeletedAt(),
+                entityRoles
         );
     }
 
     public static UserEntity toEntity(User domain) {
         if (domain == null) return null;
-
-        return new UserEntity(
+        List<RoleEntity> roleEntities = (domain.getRoles() == null)
+                ? List.of()
+                : domain.getRoles().stream()
+                    .map(RoleMapper::toEntity)
+                    .toList();
+        UserEntity entity = new UserEntity(
                 domain.getId(),
                 domain.getFirstName(),
                 domain.getLastName(),
                 domain.getEmail(),
                 domain.getPassword(),
                 domain.getHealthId(),
-                domain.getIsActive() // On passe l'état actif au constructeur
+                domain.getIsActive()
         );
+
+        entity.setRoles(roleEntities);
+        return entity;
     }
 }
