a2157c22a3425df4fe741f2bec8e4b9582ca8d44
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-12 21:25:26 +0100

    UserService: create: add roles in user creation,  update: add roles, findByEmail: add includeDeleted default value to findUserBy EMail

--- src/main/java/com/example/csdmp/app/domain/services/UserService.java
+++ src/main/java/com/example/csdmp/app/domain/services/UserService.java
@@ -1,12 +1,16 @@
 package com.example.csdmp.app.domain.services;
 
 import com.example.csdmp.app.domain.dtos.PaginatedResult;
+import com.example.csdmp.app.domain.entities.Permission;
+import com.example.csdmp.app.domain.entities.Role;
 import com.example.csdmp.app.domain.entities.User;
+import com.example.csdmp.app.domain.exceptions.BusinessException;
 import com.example.csdmp.app.domain.exceptions.EntityNotFoundException;
 import com.example.csdmp.app.domain.repositories.RoleRepository;
 import com.example.csdmp.app.domain.repositories.UserRepository;
 import com.example.csdmp.app.domain.security.PasswordInterface;
 
+import java.util.List;
 import java.util.UUID;
 
 public class UserService {
@@ -22,9 +26,15 @@ public class UserService {
         this.passwordInterface = passwordInterface;
     }
 
-    public User create(String firstName, String lastName, String email, String healthId, String password, boolean isActive){
+    public User create(String firstName, String lastName, String email, String healthId, String password, List<UUID> roleIds, boolean isActive){
+        if (userRepository.findByHealthId(healthId).isPresent()) {
+            throw new BusinessException("Un utilisateur avec le mêm numéro CNSS existe déjà");
+        }
         String hashedPassword = passwordInterface.hash(password);
-        User user = new User(UUID.randomUUID(), firstName, lastName, email, healthId, hashedPassword, isActive);
+        List<Role> roles = roleIds.stream()
+                .map(rId -> roleRepository.findById(rId).orElseThrow(() -> new EntityNotFoundException("Le role " + rId + " n'existe pas")))
+                .toList();
+        User user = new User(UUID.randomUUID(), firstName, lastName, email, healthId, hashedPassword, roles, isActive);
         userRepository.save(user);
         return user;
     }
@@ -34,8 +44,9 @@ public class UserService {
     }
 
     public User update(UUID id, String firstName, String lastName, String email, String healthId, String password, boolean isActive){
+        List<Role> roles = List.of();
         User user = this.findById(id);
-        User updatedUser = new User(user.getId(), firstName, lastName, email, healthId, password, isActive);
+        User updatedUser = new User(user.getId(), firstName, lastName, email, healthId, password, roles, isActive);
         userRepository.save(updatedUser);
         return updatedUser;
     }
@@ -47,7 +58,7 @@ public class UserService {
     }
 
     public User findByEmail(String email){
-        return userRepository.findByEmail(email).orElseThrow(
+        return userRepository.findByEmail(email, false).orElseThrow(
                 () -> new EntityNotFoundException("Utilisateur non trouvé avec l'email : " + email)
         );
     }
