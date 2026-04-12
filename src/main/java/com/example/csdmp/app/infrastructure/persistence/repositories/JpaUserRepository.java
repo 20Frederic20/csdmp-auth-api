614cd66b1eefffc37811dfdd18376e6485020152
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-12 21:16:16 +0100

    JpaUserRepository:findByEmailIncludeDeleted

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaUserRepository.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaUserRepository.java
@@ -2,6 +2,7 @@ package com.example.csdmp.app.infrastructure.persistence.repositories;
 
 import com.example.csdmp.app.infrastructure.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +10,9 @@ import java.util.UUID;
 
 @Repository
 public interface JpaUserRepository extends JpaRepository<UserEntity, UUID> {
-    Optional<UserEntity> findByEmail(String name);
+    Optional<UserEntity> findByEmail(String name, boolean includeDeleted);
     Optional<UserEntity> findByHealthId(String healthId);
+
+    @Query(value = "SELECT * FROM users WHERE email = ?", nativeQuery = true)
+    Optional<UserEntity> findByEmailIncludeDeleted(String email);
 }
