c0215f0299b01030a84922c78fad16973a684b88
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-12 21:22:30 +0100

    UserRequest: add roles in user request

--- src/main/java/com/example/csdmp/app/interfaces/rest/dtos/UserRequest.java
+++ src/main/java/com/example/csdmp/app/interfaces/rest/dtos/UserRequest.java
@@ -1,6 +1,7 @@
 package com.example.csdmp.app.interfaces.rest.dtos;
 
+import java.util.List;
 import java.util.UUID;
 
-public record UserRequest(String firstName, String lastName, String email, String healthId, String password, boolean isActive) {
+public record UserRequest(String firstName, String lastName, String email, String healthId, String password, List<UUID> roleIds, boolean isActive) {
 }
