ed1b2bc6e142f1b30441bc32a62855633ccfebf1
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-16 01:29:43 +0100

    Add role in user Response

--- src/main/java/com/example/csdmp/app/interfaces/rest/dtos/UserResponse.java
+++ src/main/java/com/example/csdmp/app/interfaces/rest/dtos/UserResponse.java
@@ -1,7 +1,9 @@
 package com.example.csdmp.app.interfaces.rest.dtos;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
-public record UserResponse(UUID id, String firstName, String lastName, String email, String healthId, String password, boolean isActive, LocalDateTime createdAT, LocalDateTime updatedAt, LocalDateTime deletedAt) {
+public record UserResponse(UUID id, String firstName, String lastName, String email, String healthId, String password,
+                           List<RoleResponse> roles, boolean isActive, LocalDateTime createdAT, LocalDateTime updatedAt, LocalDateTime deletedAt) {
 }
