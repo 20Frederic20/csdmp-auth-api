fa1de83a5999e8963b4be51eba61eb7a5f5a288b
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-17 01:10:14 +0100

    DomainConfig
    public AuthService authService

--- src/main/java/com/example/csdmp/app/infrastructure/domain/DomainConfig.java
+++ src/main/java/com/example/csdmp/app/infrastructure/domain/DomainConfig.java
@@ -1,12 +1,17 @@
 package com.example.csdmp.app.infrastructure.domain;
 
 import com.example.csdmp.app.domain.repositories.PermissionRepository;
+import com.example.csdmp.app.domain.repositories.RefreshTokenRepository;
 import com.example.csdmp.app.domain.repositories.RoleRepository;
 import com.example.csdmp.app.domain.repositories.UserRepository;
 import com.example.csdmp.app.domain.security.PasswordInterface;
+import com.example.csdmp.app.domain.security.TokenInterface;
+import com.example.csdmp.app.domain.services.AuthService;
 import com.example.csdmp.app.domain.services.PermissionService;
 import com.example.csdmp.app.domain.services.RoleService;
 import com.example.csdmp.app.domain.services.UserService;
+import com.example.csdmp.app.infrastructure.persistence.repositories.JpaRefreshTokenRepository;
+import com.example.csdmp.app.infrastructure.security.JwtTokenService;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
@@ -31,12 +36,17 @@ public class DomainConfig {
     }
 
     @Bean
-    public PermissionService permmissionService(PermissionRepository permissionRepository) {
+    public PermissionService permissionService(PermissionRepository permissionRepository) {
         return new PermissionService(permissionRepository);
     }
 
     @Bean
     public UserService userService(UserRepository userRepository, RoleRepository roleRepository, PasswordInterface passwordInterface) {
         return new UserService(userRepository, roleRepository, passwordInterface);
+    }
+
+    @Bean
+    public AuthService authService(UserRepository userRepository, PasswordInterface passwordInterface, TokenInterface tokenInterface) {
+        return  new AuthService(userRepository, passwordInterface, tokenInterface);
     }
 }
