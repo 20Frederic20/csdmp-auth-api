cb3a7d9fd07fd305d1d9adca171c28ebb95ffd53
20Frederic20 <fredericpierrothountinme@gmail.com>
2026-04-17 01:03:08 +0100

    add Repository annoation in JpaPermissionRepository

--- src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaPermissionRepository.java
+++ src/main/java/com/example/csdmp/app/infrastructure/persistence/repositories/JpaPermissionRepository.java
@@ -2,10 +2,12 @@ package com.example.csdmp.app.infrastructure.persistence.repositories;
 
 import com.example.csdmp.app.infrastructure.persistence.entities.PermissionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface JpaPermissionRepository extends JpaRepository<PermissionEntity, UUID> {
     Optional<PermissionEntity> findByResourceAndAction(String resource, String action);
 }
